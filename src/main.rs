987667f22776a34cc6a68d214162ff7bd891e6cb
spixa <spixa@github.com>
2023-08-02 19:03:45 +0330

    Added all moded operations and NOT

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 const ADD: u8 = 0x01;
 const SUB: u8 = 0x02;
 const JMP: u8 = 0x03;
@@ -45,7 +47,7 @@ impl Program {
         println!("Advancing to next byte\n\tcounter: {}", self.counter);
 
         for (idx, i) in self.regs.into_iter().enumerate() {
-            println!("\treg{idx}:    {}", i);
+            println!("\treg{idx}:    {:#010} ({:#034b}) ({:#010x})", i, i, i);
         }
 
         self.counter += 1;
@@ -80,6 +82,72 @@ impl Program {
         ((array[0] as u32) << 16) | array[1] as u32
     }
 
+    fn modded_instr(&mut self, which: u8, mode: u8) {
+        let target = self.next_byte();
+
+        assert!(target < 4, "TARGET more than allowed");
+
+        let source: u32 = match mode {
+            RR_MODE => {
+                let other_register = self.next_byte();
+
+                assert!(other_register < 4, "REGISTER_ID more than allowed");
+                self.regs[other_register as usize]
+            }
+            RB_MODE => {
+                let byte = self.next_byte();
+
+                byte as u32
+            }
+            RW_MODE => {
+                let word = self.next_word();
+
+                word as u32
+            }
+            RD_MODE => self.next_dword(),
+            _ => todo!(),
+        };
+
+        match which {
+            AND => {
+                self.regs[target as usize] &= source;
+            }
+            NAND => {
+                self.regs[target as usize] = !(self.regs[target as usize] & source);
+            }
+            OR => {
+                self.regs[target as usize] |= source;
+            }
+            NOR => {
+                self.regs[target as usize] = !(self.regs[target as usize] | source);
+            }
+            XOR => {
+                self.regs[target as usize] ^= source;
+            }
+            XNOR => {
+                self.regs[target as usize] = !(self.regs[target as usize] ^ source);
+            }
+            MOV => {
+                self.regs[target as usize] = source;
+            }
+            ADD => {
+                self.regs[target as usize] += source;
+            }
+            SUB => {
+                self.regs[target as usize] -= source;
+            }
+            CMP => {
+                // for jmz and jlz we will be using 2 and 1 respectively
+                self.regs[0] = match self.regs[target as usize].cmp(&source) {
+                    Ordering::Less => 1,
+                    Ordering::Equal => 0,
+                    Ordering::Greater => 2,
+                };
+            }
+            _ => todo!(),
+        }
+    }
+
     fn execute(&mut self) {
         while !self.halted {
             if self.counter as usize >= self.code.len() {
@@ -89,70 +157,26 @@ impl Program {
             let byte = self.code[self.counter as usize];
 
             match byte {
-                ADD => {}
-                SUB => {}
                 JMP => {}
                 JZ => {}
                 JLZ => {}
                 JMZ => {}
-                MOV => {
-                    let mode = self.next_byte();
-                    let register_id = self.next_byte();
-                    assert!(register_id < 4, "REGISTER_ID more than allowed");
-
-                    match mode {
-                        RR_MODE => {
-                            let other_register = self.next_byte();
-
-                            assert!(register_id < 4, "REGISTER_ID more than allowed");
-                            self.regs[register_id as usize] = self.regs[other_register as usize];
-                            println!(
-                                "Set register-{register_id} to value of register-{other_register}"
-                            );
-                        }
-                        RB_MODE => {
-                            let byte = self.next_byte();
-
-                            self.regs[register_id as usize] = byte as u32;
-
-                            println!(
-                                "Set register-{register_id} to the casted value of byte {byte}"
-                            );
-                        }
-                        RW_MODE => {
-                            let word = self.next_word();
-
-                            self.regs[register_id as usize] = word as u32;
-
-                            println!(
-                                "Set register-{register_id} to the casted value of wword {word}"
-                            );
-                        }
-                        RD_MODE => {
-                            let dword = self.next_dword();
-
-                            self.regs[register_id as usize] = dword;
-
-                            println!(
-                                "Set register-{register_id} to the casted value of dword {dword}"
-                            );
-                        }
-                        _ => todo!(),
-                    }
-                }
                 LDP => {}
                 STP => {}
-                AND => {}
-                NOT => {}
-                OR => {}
-                NOR => {}
-                NAND => {}
-                XOR => {}
-                XNOR => {}
+                AND | NAND | OR | NOR | XOR | XNOR | MOV | ADD | SUB | CMP => {
+                    let mode = self.next_byte();
+
+                    self.modded_instr(byte, mode);
+                }
+                NOT => {
+                    let reg = self.next_byte();
+                    assert!(reg < 4, "TARGET more than allowed");
+
+                    self.regs[reg as usize] = !self.regs[reg as usize];
+                }
                 HLT => {}
                 NOP => {}
                 INT => {}
-                CMP => {}
                 _ => todo!(),
             }
             self.step();
@@ -161,7 +185,8 @@ impl Program {
 }
 
 fn main() {
-    let thing: Vec<u8> = vec![7, 4, 1, 0x01, 0x01, 0x00, 0x00, 7, 1, 0, 1];
+    let thing: Vec<u8> = vec![MOV, RB_MODE, 0, 12, MOV, RB_MODE, 1, 11, CMP, RR_MODE, 1, 0];
+
     let mut p1 = Program::new(thing);
     p1.execute();
 }
